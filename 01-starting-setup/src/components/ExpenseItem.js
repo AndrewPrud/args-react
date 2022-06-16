ea7688172aa6cfcc16dd14107f7b0657d4c3ee8d
lw902 <lilliankwalbrun@gmail.com>
2022-06-16 13:36:17 -0500

    updates

--- 01-starting-setup/src/components/ExpenseItem.js
+++ 01-starting-setup/src/components/ExpenseItem.js
@@ -1,11 +1,17 @@
 import React from 'react';
-
+import './ExpenseItem.css';
 function ExpenseItem(){
+    const expenseDate = new Date(2022, 2, 28);
+    const expenseTitle = 'Hulu Subscription';
+    const expenseAmount = '$30.00';
+
     return(
-        <div>
-            <h1>
-                Expenses
-            </h1>
+        <div className = 'expense-item'>
+            <div>{expenseDate.toISOString()}</div>
+            <h2>
+                {expenseTitle}
+            </h2>
+            <div>{expenseAmount}</div>
         </div>
     );
 }
