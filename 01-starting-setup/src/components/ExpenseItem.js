7df0e87ea55d46f13aeb836493625b23efcb5d23
Andrew Prudlow <andrewprudlow@gmail.com>
2022-06-16 13:06:15 -0500

    Css changes

--- 01-starting-setup/src/components/ExpenseItem.js
+++ 01-starting-setup/src/components/ExpenseItem.js
@@ -1,11 +1,14 @@
 import React from 'react';
+import './ExpenseItem.css';
 
 function ExpenseItem() {
     return (
         <div>
+            <div className='expense-item'>June 16, 2022</div>
             <h1>
-                Expenses
+                Netflix Subscription
             </h1>
+            <div className='expense-item'>$20.00</div>
         </div>
     );
 }
