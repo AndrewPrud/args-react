c0757673dc67bc363a1bcceaf8e2f785f58b6d6d
Andrew Prudlow <andrewprudlow@gmail.com>
2022-06-16 12:34:57 -0500

    2

---
+++ 01-starting-setup/src/components/ExpenseItem.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function ExpenseItem() {
+    return (
+        <div>
+            <h1>
+                Expenses
+            </h1>
+        </div>
+    );
+
+}
+
+export default ExpenseItem;
