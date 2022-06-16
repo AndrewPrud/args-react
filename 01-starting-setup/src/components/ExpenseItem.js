105189bff24e84ef1d7d630a66d79a78646406a9
Aniruddh <aniruddhmayya@gmail.com>
2022-06-16 13:12:50 -0500

    Edited ExpenseItem

--- 01-starting-setup/src/components/ExpenseItem.js
+++ 01-starting-setup/src/components/ExpenseItem.js
@@ -1,11 +1,13 @@
 import React from 'react';
+import './ExpenseItem.css';
 
 function ExpenseItem(){
     return (
-        <div>
-            <h1>
-                Expenses
-            </h1>
+        <div className="expense-item"><div className="expense-item__description">June 16, 2022</div>
+            <h2>
+                Expense 1
+            </h2>
+            <div className="expense-item__price">$3,000,000,021.</div>
         </div>
     );
 }
--- 01-starting-setup/src/index.js
+++ 01-starting-setup/src/index.js
@@ -2,6 +2,8 @@ import ReactDOM from 'react-dom/client';
 import React from 'react';
 import './index.css';
 import App from './App';
+//import ExpenseItem from './components/ExpenseItem';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<App />);
+//root.render(<ExpenseItem />);
