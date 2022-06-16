f0ff5cbf956e49fdcf7d5b97c2495a3ad701f47b
Jihong <jihongbae2nd@gmail.com>
2022-06-16 12:34:13 -0500

    init exspense

---
+++ 01-starting-setup/src/componets/ExspenseItem.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function ExspenseItem() {
+    return(
+        <div>
+            <h1>
+                Exspenses
+            </h1>
+        </div>
+    );
+}
+
+export default ExspenseItem;
