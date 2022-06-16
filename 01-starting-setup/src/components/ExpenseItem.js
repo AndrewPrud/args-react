9c29d9279c51189b0d7c3aad004de6e889eddd8f
lw902 <lilliankwalbrun@gmail.com>
2022-06-16 12:42:05 -0500

    expense Item

---
+++ 01-starting-setup/src/components/ExpenseItem.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function ExpenseItem(){
+    return(
+        <div>
+            <h1>
+                Expenses
+            </h1>
+        </div>
+    );
+}
+
+export default ExpenseItem;
