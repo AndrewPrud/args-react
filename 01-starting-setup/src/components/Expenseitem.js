28283d4692ba10e70286a1024006dfa55f5d56c6
Aniket Singh <asingh11771@gmail.com>
2022-06-16 12:33:58 -0500

    ExpenseItem

---
+++ 01-starting-setup/src/components/Expenseitem.js
@@ -0,0 +1,17 @@
+import  React  from "react";
+
+    function ExpenseItem(){
+        return(
+            <div>
+                <h1> Expenses
+
+                </h1>
+            </div>
+
+
+        )
+           
+
+
+}
+export default  ExpenseItem;
