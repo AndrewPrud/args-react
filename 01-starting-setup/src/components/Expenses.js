7bc18ab89ea472e8fdc06cc6876161e1fd808e73
Aniruddh <aniruddhmayya@gmail.com>
2022-06-17 10:15:22 -0500

    Added Expenses

---
+++ 01-starting-setup/src/components/Expenses.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import './ExpenseItem';
+import ExpenseItem from './ExpenseItem';
+import './Expenses.css';
+
+function Expenses(props){
+    return (
+    <div class='expenses'>
+      <ExpenseItem
+        title={props.in[0].title}
+        amount={props.in[0].amount}
+        date={props.in[0].date}
+      ></ExpenseItem>
+      <ExpenseItem
+        title={props.in[1].title}
+        amount={props.in[1].amount}
+        date={props.in[1].date}
+      ></ExpenseItem>
+      <ExpenseItem
+        title={props.in[2].title}
+        amount={props.in[2].amount}
+        date={props.in[2].date}
+      ></ExpenseItem>
+      <ExpenseItem
+        title={props.in[3].title}
+        amount={props.in[3].amount}
+        date={props.in[3].date}
+      ></ExpenseItem>
+    </div>
+    );
+}
+
+export default Expenses;
