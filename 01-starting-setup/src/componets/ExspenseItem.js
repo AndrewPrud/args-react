468e943cece4c391fa4903ae1d0a1ab6bf3afd42
Jihong <jihongbae2nd@gmail.com>
2022-06-17 09:39:01 -0500

    init expenseDate

--- 01-starting-setup/src/componets/ExspenseItem.js
+++ 01-starting-setup/src/componets/ExspenseItem.js
@@ -1,19 +1,13 @@
-/* eslint-disable */
 import React from "react";
 import './ExspenseItem.css';
+import './ExpenseDate.js';
+import ExspenceDate from "./ExpenseDate";
 
 function ExspenseItem(props) {
-    const month = props.date.toLocaleString('en-US', {month: 'long'});
-    const date = props.date.toLocaleString('en-US', {day: '2-digit'});
-    const year = props.date.getFullYear();
 
     return (
-        <div className="expence-item"> 
-            <div>
-                <div>{month}</div>
-                <div>{year}</div>
-                <div>{date}</div>
-            </div>
+        <div className="expence-item">
+            <ExspenceDate date={props.date}></ExspenceDate> 
             <div className="expense-item__description">
                 <h2>{props?.title}</h2>
             </div>
