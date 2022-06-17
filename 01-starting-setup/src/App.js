3418c6aef4863ebca58776876e716dceb4fd4a06
Andrew Prudlow <andrewprudlow@gmail.com>
2022-06-17 10:59:38 -0500

    Small changes to price format

--- 01-starting-setup/src/App.js
+++ 01-starting-setup/src/App.js
@@ -5,25 +5,25 @@ function App() {
       id: "e1",
       title: "Paper",
       date: new Date(2022, 6, 16),
-      amount: 20.00
+      amount: 20.99
     },
     {
       id: "e2",
       title: "Desk",
       date: new Date(2022, 4, 16),
-      amount: 200.00
+      amount: 200.50
     },
     {
       id: "e3",
       title: "Lamp",
       date: new Date(2022, 3, 16),
-      amount: 12.00
+      amount: 12.25
     },
     {
       id: "e4",
       title: "TV",
       date: new Date(2022, 5, 19),
-      amount: 2000.00
+      amount: 2000.12
     }
   ]
   
