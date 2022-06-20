d9c9eea40be6d949f050e3c83d80a50138bca16d
Aniket Singh <asingh11771@gmail.com>
2022-06-20 09:41:06 -0500

    Title UX 1

--- 01-starting-setup/src/components/Expenseitem.js
+++ 01-starting-setup/src/components/Expenseitem.js
@@ -1,20 +1,25 @@
-import  React  from "react";
+import  React, {useState}  from "react";
 import "./Expenseitem.css";
 import "./Expensedate.js"
 import "./card.js";
 import Expensedate from "./Expensedate.js";
 import Card from "./card.js";
     function ExpenseItem(props){
- 
+        const[title, setTitle] = useState(props.title);
+
+    function clickHandled(){
+        setTitle('Update');
+    }
         return(
             <Card className = 'expense-item'>
                 <Expensedate date = {props.date}></Expensedate>
                 <div className ="expense-item__description"> 
-                    <h2>{props.title}</h2>
+                    <h2>{title}</h2>
                 </div>
                 <div className="expense-item__price">
                     {props.amount}
                 </div>
+                <button onClick = {clickHandled}>Change Title</button>
             </Card>
                 
                 
