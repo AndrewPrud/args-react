9c660098a1fd3350fbf4e2c51dcb674fdd50e811
Aniket Singh <asingh11771@gmail.com>
2022-06-20 13:02:32 -0500

    Filter

--- 01-starting-setup/src/components/NewExpense/Expensesfilter.js
+++ 01-starting-setup/src/components/NewExpense/Expensesfilter.js
@@ -3,12 +3,14 @@ import React from 'react';
 import './Expensefilter.css';
 
 const ExpensesFilter = (props) => {
-
+    function dropDownChangeHandler(event){
+        props.onFilterChange(event.target.value);
+    }
   return (
     <div className='expenses-filter'>
       <div className='expenses-filter__control'>
         <label>Filter by year</label>
-        <select>
+        <select onChange = {dropDownChangeHandler}>
           <option value='2022'>2022</option>
           <option value='2021'>2021</option>
           <option value='2020'>2020</option>
