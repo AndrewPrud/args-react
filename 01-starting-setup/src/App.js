1542b4ce2517e7f957be75c22f80ba05cce74edc
Aniruddh <aniruddhmayya@gmail.com>
2022-06-20 15:04:56 -0500

    Lists

--- 01-starting-setup/src/App.js
+++ 01-starting-setup/src/App.js
@@ -1,8 +1,8 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Expenses from './components/Expenses';
 import NewExpense from './components/newExpense/NewExpense.js';
 function App() {
-  const expenses = [{
+  const initialExpenses = [{
     id: 'e1',
     title: 'Toilet Paper',
     amount: 94.12,
@@ -26,10 +26,19 @@ function App() {
     amount: 30000.19,
     date: new Date(2022, 7, 16),
   }
-  ]
+  ];
+
+  const[expenses, setExpenses] = useState(initialExpenses);
+
+  function addExpenseHandler(expense){
+    setExpenses((prevExpenses) => {
+      return[...prevExpenses, expense]
+    });
+    }
+  
   return (
     <div>
-      <NewExpense></NewExpense>
+      <NewExpense onAddExpense={addExpenseHandler}></NewExpense>
       <Expenses in={expenses}></Expenses>
     </div>
   );
